bb3d82a8218a7cabbfe64b8d97f790c363cac54c
HammadSiddique <hammad.siddique01@gmail.com>
2022-06-07 20:38:43 +0500

    Add unique key

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
+import {v4 as uuidv4} from 'uuid';
 
 class TodoContainer extends React.Component {
   state = {
@@ -47,7 +48,7 @@ class TodoContainer extends React.Component {
   }
   addTodoItem = title => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false
     };
