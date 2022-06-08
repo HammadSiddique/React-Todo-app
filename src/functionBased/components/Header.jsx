cfa498ce6688b3836f5321732d320f612c392b4c
HammadSiddique <hammad.siddique01@gmail.com>
2022-06-08 13:20:14 +0500

    Fix linter errors

--- src/functionBased/components/Header.jsx
+++ src/functionBased/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const Header = () => {
   const headerStyle = {
@@ -8,13 +8,13 @@ const Header = () => {
   return (
     <header style={headerStyle}>
       <h1 style={{
-                fontSize: "6rem",
-                fontWeight: "600",
-                marginBottom: "2rem",
-                lineHeight: "1em",
-                color: "#ececec",
-                textTransform: "lowercase",
-                textAlign: "center",
+                fontSize: '6rem',
+                fontWeight: '600',
+                marginBottom: '2rem',
+                lineHeight: '1em',
+                color: '#ececec',
+                textTransform: 'lowercase',
+                textAlign: 'center',
       }}>todos</h1>
     </header>
   );
